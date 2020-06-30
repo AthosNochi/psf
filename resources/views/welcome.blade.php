99f52446d7692260af0cc21c2a7d19de2d92f910
Athos Nochi <athosnochi@gmail.com>
2020-06-30 15:45:22 -0300

    homepage alterado v.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
 {!! Form :: open(['route' => 'user.login', 'method' => 'get']) !!}
 {!! Form::submit('Login') !!}
-=======
-Max gay
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
->>>>>>> master
+
 
 {!! Form::close() !!}
