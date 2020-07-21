1a68be2f0643f70dd2887245d77c6bca8717b43e
Athos Nochi <athosnochi@gmail.com>
2020-07-21 19:13:02 -0300

    correção controlleragenda

--- app/Entities/Agenda.php
+++ app/Entities/Agenda.php
@@ -3,7 +3,6 @@
 namespace App\Entities;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
