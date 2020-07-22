1219128e5f1a7b9b810ef8412a5d0199fab46561
Athos Nochi <athosnochi@gmail.com>
2020-07-22 20:23:31 -0300

    .

--- resources/views/templates/menu-lateral.blade.php
+++ resources/views/templates/menu-lateral.blade.php
@@ -13,7 +13,7 @@
             </a>
         </li>
         <li>
-            <a href="{{ route('agenda.lista') }}">
+            <a href="{{ route('agenda.index') }}">
                 <i class"""></i>
                 <h3>Agenda</h3>
             </a>
