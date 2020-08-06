23dea0144d1736a7414d9dc10d4d7413aec9d231
Athos Nochi <athosnochi@gmail.com>
2020-08-06 17:34:46 -0300

    .

--- app/Entities/Doctor.php
+++ app/Entities/Doctor.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * Class Doctor.
