e5f25021a99f6a612649de13682d50ce7e77dee2
Athos Nochi <athosnochi@gmail.com>
2020-08-06 17:47:00 -0300

    alterações de nome na view

--- resources/views/templates/menu-lateral.blade.php
+++ resources/views/templates/menu-lateral.blade.php
@@ -9,7 +9,7 @@
         <li>
         <a href="{{ route('psf.index') }}">
                 <i class"""></i>
-                <h3>PSF</h3>
+                <h3>Psf</h3>
             </a>
         </li>
         <li>
@@ -21,7 +21,7 @@
         <li>
             <a href="">
                 <i class"""></i>
-                <h3>Agenda</h3>
+                <h3>Agendamento</h3>
             </a>
         </li>
     </ul>
