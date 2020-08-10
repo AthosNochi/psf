10f22b066d641a043dde3136d87db8d3adfd0019
Athos Nochi <athosnochi@gmail.com>
2020-08-10 20:05:57 -0300

    datetime-local pronto

--- resources/views/agendamentos/form.blade.php
+++ resources/views/agendamentos/form.blade.php
@@ -4,14 +4,15 @@
 
     {!! Form::open(['route' => 'agendamento.store', 'method' => 'post', 'class' => 'form-padrao']) !!}
         @include('templates.formulario.input', ['input' => 'descricao', 'attributes' => ['placeholder' => "Descrição"]])
+
+        <label for="datahora">Data/hora:
+        <input type="datetime-local" name="datahora"/>
+        </label>
         
         @include('templates.formulario.select', ['select' => 'id_patient', 'data' => $patient_list, 'attributes' => ['placeholder' => "Paciente"]])
         @include('templates.formulario.select', ['select' => 'id_doctor', 'data' => $doctor_list, 'attributes' => ['placeholder' => "Medico"]])               
         @include('templates.formulario.input', ['input' => 'legenda', 'attributes' => ['placeholder' => "Legenda"]])
         
-
-        <label for="birthdaytime">(date and time):</label>
-        <input type="datetime-local" id="datahora" value="datahora" name="birthdaytime">
 
          <!-- <input id="datahora" type="datetime-local" name="birthdaytime"> -->
         
