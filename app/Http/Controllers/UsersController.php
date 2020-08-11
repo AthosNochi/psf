938424a22d9cea80dcbe07a0b11fb095efdba1d5
Athos Nochi <athosnochi@gmail.com>
2020-08-11 15:12:54 -0300

    .

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -58,7 +58,7 @@ class UsersController extends Controller
     public function store(UserCreateRequest $request)
     {
         //$request = $this->service->store($request->all());//
-        $isAdm = $isAdm[''];
+        $isAdm = $isAdm['$_POST'];
         if(!strcmp ( $isAdm , 'on' ))
             $isAdm=1;
         else
