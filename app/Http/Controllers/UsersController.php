2668ec64154f3d2d242699fb1cf1af598b8f0f1f
Athos Nochi <athosnochi@gmail.com>
2020-08-11 14:36:46 -0300

    .

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -39,7 +39,7 @@ class UsersController extends Controller
 
     public function index()
     {
-        $isAdm ['isAdm'];
+        $isAdm ['$isAdm'];
         $users = $this->repository->all();
         return view('user.index')->with([
             'users'=>$users,
