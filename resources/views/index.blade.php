aa675b17ab013ed2a99faf0acbd2aabff6e26b6d
Athos Nochi <athosnochi@gmail.com>
2020-08-17 16:54:25 -0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
     <nav class="navbar navbar-light bg-light static-top">
       <div class="container">
         <a class="navbar-brand" href="#">Sistema de Agendamento</a>
-        <a class="btn btn-primary" href="{{ url('/entrar') }}">Entrar</a>
+        <a class="btn btn-primary" href="{{ url('/login') }}">Entrar</a>
       </div>
     </nav>
 
