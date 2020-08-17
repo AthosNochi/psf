02f9e66dc790b8788932a8b6be149c4e241f38d6
Athos Nochi <athosnochi@gmail.com>
2020-08-17 20:04:11 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -17,43 +17,27 @@ Auth::routes();
 
 Route::get('/', 'IndexController@index');
 Route::get('/entrar', 'IndexController@login');
+Route::get('/cadastrar', 'IndexController@register');
+
 Route::get('/home', 'HomeController@principal');
-Route::get('/cadastro', ['uses' => 'Controller@cadastrar']);
-
-Route :: group ([ 'middleware' => 'auth' ], function () {
-    Route :: get ( '/ login / logout' , [ 'as' => 'login.logout' , 'uses' => 'LoginController@logout' ]);
-    Route :: get ( '/ changePassword' , [ 'as' => 'login.changePassword' , 'uses' => 'LoginController@changePassword' ]);
-    Route :: post ( '/ changePassword / save' , [ 'as' => 'login.saveNewPassword' , 'uses' => 'LoginController@savePassword' ]);
-  });
-
-/*
-------------------------------
-Rotas de parte administrativa
-------------------------------
-*/
-Route::group(['middleware'=>'admin'],function(){
-Route::get('/admin',['as'=>'user.index','uses'=>'Controller@index']);
-  
-});
-
-/**
-----------------------------------------------------------------------------
-routes to user auth
-----------------------------------------------------------------------------
-**/
-
-Route::get('/login', ['uses' => 'LoginController@fazerLogin']);
-Route::post('/login', ['as' => 'user.login', 'uses' => 'DashboardController@auth']);
-Route::get('/dashboard', ['as' => 'user.dashboard', 'uses' => 'DashboardController@index']);
 
 
+Route::resource('/user', 'UsersController');
 
-Route::resource('user', 'UsersController');
-Route::resource('psf', 'PsfsController');
-Route::resource('doctor', 'DoctorsController');
-Route::resource('patient', 'PatientsController');
-Route::resource('agendamento', 'AgendamentosController');
+Route::resource('/psf', 'PsfsController');
+
+Route::resource('/doctor', 'DoctorsController');
+
+Route::resource('/patient', 'PatientsController');
+
+Route::resource('/agendamento', 'AgendamentosController');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
