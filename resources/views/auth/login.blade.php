885ba4293f6fc196d4b70d22732154991bcca109
Athos Nochi <athosnochi@gmail.com>
2020-08-17 20:25:14 -0300

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" action="{{ route('/entrar') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group row">
