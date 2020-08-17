46fb9515f49f0ba7ae407207d2bab24638ef1f18
Athos Nochi <athosnochi@gmail.com>
2020-08-17 17:03:39 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', 'IndexController@index');
 Route::get('/cadastrar', 'IndexController@register');
 
 Route::get('/login', ['uses' => 'Controller@fazerLogin']);
-Route::post('/login', ['as' => 'user.login', 'uses' => 'Controller@auth']);
+Route::post('/login', ['as' => 'user.login', 'uses' => 'DashboardController@auth']);
 
 Route::resource('/user', 'UsersController');
 
