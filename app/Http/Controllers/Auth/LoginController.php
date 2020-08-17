322485ad58b410449fd966df481b38688364bba5
Athos Nochi <athosnochi@gmail.com>
2020-08-17 11:16:28 -0300

    .

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,24 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 use Illuminate\Http\Request;
-use App\Repositories\UserRepository;
-use App\Validators\UserValidator;
+use App\Entities\User;
 
 class LoginController extends Controller
 {
-    private $repository;
-	private $validator;
-
-    public function __construct(UserRepository $repository, UserValidator $validator)
-    {
-        $this->repository = $repository;
-        $this->validator  = $validator;
-    }
-
 
     public function index(){
     	return view('user.index');    
@@ -27,41 +15,19 @@ class LoginController extends Controller
 
     public function login(Request $request){
     	
-    	$data = [
+        $data = [
     		'email' => $request->get('username'),
     		'password' => $request->get('password')
-    	];
-
-    	try
-    	{
-            if(env('PASSWORD_HASH'))
-            {
-                \Auth::attempt($data, false);
-            }
-
-    		else
-            {
-               $user = $this->repository->findWhere(['email' => $request->get('username')])->first();
-
-
-                if(!$user)
-                    throw new \Exception("O email informado é invalido");
-
-
-               if($user->password == $request->get('password'))
-                throw new \Exception("A senha informada é invalida");
-
-                \Auth::login($user);
-            }
-
-    		return redirect()->route('user.index');
-    	}
-		catch (\Exception $e)
-    	{
-    		return $e->getMessage();
-    	}
-
-    	dd($request->all());
+        ];
+        
+        if(Auth::attempt(['email'=>$data->email,'password'=>$data->password])){
+          if(Auth::user()->isAdm){
+            return redirect()->route('user.index');
+          }else{
+            return redirect()->route('userN.index');
+          }
+        }
+        return redirect()->route('login.index');
     	
     }
 }
