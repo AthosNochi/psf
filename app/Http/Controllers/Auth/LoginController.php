1907f97b1e1b65c0548dce849e312494171b18b8
Athos Nochi <athosnochi@gmail.com>
2020-08-17 10:48:43 -0300

    .

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\Repositories\UserRepository;
+use App\Validators\UserValidator;
 
 class LoginController extends Controller
 {
