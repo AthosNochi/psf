229452146956e6189713299ba97ccec153d5e0b6
Athos Nochi <athosnochi@gmail.com>
2020-08-17 15:54:16 -0300

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function create(array $data, UserCreateRequest $request)
     {
          //$request = $this->service->store($request->all());//
          if(isset($request->isAdm) && !strcmp($request->isAdm,'on')){
