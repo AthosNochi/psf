1ace0985e21ff1fe57b27570a0233ef77930408e
Athos Nochi <athosnochi@gmail.com>
2020-08-17 17:49:26 -0300

    .

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,6 +1,28 @@
 <!DOCTYPE html>
-{!! Form :: open(['route' => 'user.login', 'method' => 'get']) !!}
-{!! Form::submit('Login') !!}
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+    </head>
+
+    <body>
+
+    <section id="conteudo-view" class="login">
+        {!! Form::open(['route' => 'user.login', 'method' => 'get']) !!}
+
+        {!! Form::text('username', null, ['class' => 'input', 'placeholder' => "Usuario"]) !!}
+        {!! Form::password('password', ['palceholder' => "Senha"])}
+
+        {!! Form::submit('Login') !!}
 
 
-{!! Form::close() !!}
+        {!! Form::close() !!}
+    
+    <form class="" action="index.html" method="post">
+
+  
+    </section>
+
+    </body>
+</html>
