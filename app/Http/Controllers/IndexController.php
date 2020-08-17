b23aa11acc5628303c28ed2063ce5e1fa14d882a
Athos Nochi <athosnochi@gmail.com>
2020-08-17 16:21:11 -0300

    .

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,7 +11,7 @@ class IndexController extends Controller
     }
 
     public function login(){
-        return view('auth.login');
+        return view('user.index');
     }
 
     public function register(){
