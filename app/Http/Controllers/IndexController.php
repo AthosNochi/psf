70d6c760449f0fd3389b577a79de44ce1feb0c61
Athos Nochi <athosnochi@gmail.com>
2020-08-17 17:12:29 -0300

    .

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class IndexController extends Controller
 {
     public function index(){
-        return view('login.app');
+        return view('auth.login');
     }
 
     public function login(){
