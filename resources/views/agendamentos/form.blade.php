d3d1e9911a1d51296e840714d3856cbe23184d69
Athos Nochi <athosnochi@gmail.com>
2020-08-18 17:21:25 -0300

    .

--- resources/views/agendamentos/form.blade.php
+++ resources/views/agendamentos/form.blade.php
@@ -40,38 +40,56 @@
       @include('templates.formulario.submit', ['input' => 'Salvar'])
     {!! Form::close() !!}
 
-    <table class="default-table">
-      <thead>
-          <tr>
-              <td>#</td>
-              <td>Descrição</td>
-              <td>Data/Hora</td>
-              <td>Paciente</td>
-              <td>Médico</td>
-              <td>legenda</td>
-              <td>Menu</td>
-          </tr>
-      </thead>
-      <tbody>
-          @foreach ($agendamentos as $agendamento)
-          <tr>
-              <td>{{ $agendamento->id }}</td>
-              <td>{{ $agendamento->descricao }}</td>
-              <td>{{ $agendamento->datahora }}</td>
-              <td>{{ $agendamento->patient }}</td>
-              <td>{{ $agendamento->doctor }}</td>
-              <td>{{ $agendamento->legenda }}</td>
+    @foreach ($agendamentos as $agendamento)
+        <table id="agendamento_table" class="table table-striped table-hover" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Data/Hora</th>
+                    <th>Paciente</th>
+                    <th>Descrição</th>
+                    <th>Médico</th>
+                    <th>Especialidade</th>
+                    <th>Editar</th>
+                    <th>Excluir</th>
+                    <th>Legenda</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($agendamentos as $agendamento)
+                <tr>
+                    <td>{{ $agendamento->id }}</td>
+                    <td>{{ date("d/m/Y H:i:s", strtotime($agendamento->datahora)) }}</td>
+                    <td>{{ $agendamento->patient->name }}</td>
+                    <td>{{ $agendamento->descricao }}</td>
+                                
+                    <td>{{ $agendamento->doctor->name }}</td>
+                    <td>{{ $agendamento->doctor->specialty }}</td>
+                    <td><a href="{{ route('agendamentos.edit', $agendamento->id) }}" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></a></td>
+                        <td>
+                        <form action="{{ route('agendamentos.destroy', $agendamento->id ) }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-danger" type="submit"><span class="glyphicon glyphicon-trash"></button>
+                        </form>
+                    </td> 
+                        <td>{{ $agendamento->legenda }}</td>
+                <!-- <td><button class="btn btn-info demo" >Status</button></td> -->        
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endforeach
 
-              <td>
-                  {!! Form::open(['route' => ['agendamento.destroy', $agendamento->id], 'method' => 'DELETE']) !!}
-                  {!! Form::submit('Remover') !!}
-                  {!! Form::close() !!}
-              </td>
-          </tr>
-          @endforeach
-      </tbody>
-  </table>
-                
-            
-        
+ <!-- Footer -->
+    <footer class="footer bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
+            <p class="text-muted small mb-4 mb-lg-0">&copy; Renato de Oliveira Lucena 2018</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+      
 @endsection
