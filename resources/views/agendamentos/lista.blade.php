cfc82b55bc28ba39c5434f346f9bc37cddb5d91f
Athos Nochi <athosnochi@gmail.com>
2020-08-18 17:20:28 -0300

    .

--- resources/views/agendamentos/lista.blade.php
+++ resources/views/agendamentos/lista.blade.php
@@ -4,57 +4,46 @@
 <h3>{{ session('success')['messages'] }}</h3>
 @endif
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    @foreach ($agendamentos as $agendamento)
-                    <table id="agendamento_table" class="table table-striped table-hover" cellspacing="0" width="100%">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Data/Hora</th>
-                                <th>Paciente</th>
-                                <th>Descrição</th>
-                                <th>Médico</th>
-                                <th>Especialidade</th>
-                                <th>Editar</th>
-                                <th>Excluir</th>
-                                <th>Legenda</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($agendamentos as $agendamento)
-                            <tr>
-                                <td>{{ $agendamento->id }}</td>
-                                <td>{{ date("d/m/Y H:i:s", strtotime($agendamento->datahora)) }}</td>
-                                <td>{{ $agendamento->patient->name }}</td>
-                                <td>{{ $agendamento->descricao }}</td>
+    @foreach ($agendamentos as $agendamento)
+        <table id="agendamento_table" class="table table-striped table-hover" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Data/Hora</th>
+                    <th>Paciente</th>
+                    <th>Descrição</th>
+                    <th>Médico</th>
+                    <th>Especialidade</th>
+                    <th>Editar</th>
+                    <th>Excluir</th>
+                    <th>Legenda</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($agendamentos as $agendamento)
+                <tr>
+                    <td>{{ $agendamento->id }}</td>
+                    <td>{{ date("d/m/Y H:i:s", strtotime($agendamento->datahora)) }}</td>
+                    <td>{{ $agendamento->patient->name }}</td>
+                    <td>{{ $agendamento->descricao }}</td>
                                 
-                                <td>{{ $agendamento->doctor->name }}</td>
-                                <td>{{ $agendamento->doctor->specialty }}</td>
-                                <td><a href="{{ route('agendamentos.edit', $agendamento->id) }}" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></a></td>
-                                 <td>
-                                    <form action="{{ route('agendamentos.destroy', $agendamento->id ) }}" method="POST">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-                                        <button class="btn btn-danger" type="submit"><span class="glyphicon glyphicon-trash"></button>
-                                    </form>
-                                </td> 
-                                    <td>{{ $agendamento->legenda }}</td>
-                            <!-- <td><button class="btn btn-info demo" >Status</button></td> -->        
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @endforeach
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                    <td>{{ $agendamento->doctor->name }}</td>
+                    <td>{{ $agendamento->doctor->specialty }}</td>
+                    <td><a href="{{ route('agendamentos.edit', $agendamento->id) }}" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></a></td>
+                        <td>
+                        <form action="{{ route('agendamentos.destroy', $agendamento->id ) }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-danger" type="submit"><span class="glyphicon glyphicon-trash"></button>
+                        </form>
+                    </td> 
+                        <td>{{ $agendamento->legenda }}</td>
+                <!-- <td><button class="btn btn-info demo" >Status</button></td> -->        
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        @endforeach
 
  <!-- Footer -->
     <footer class="footer bg-light">
