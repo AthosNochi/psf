4660d6aabcd68cb2925d6bcb3e1f93c652a637c9
Athos Nochi <athosnochi@gmail.com>
2020-08-18 17:33:34 -0300

    .

--- resources/views/agendamentos/form.blade.php
+++ resources/views/agendamentos/form.blade.php
@@ -40,46 +40,6 @@
       @include('templates.formulario.submit', ['input' => 'Salvar'])
     {!! Form::close() !!}
 
-    @foreach ($agendamentos as $agendamento)
-        <table id="agendamento_table" class="table table-striped table-hover" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Data/Hora</th>
-                    <th>Paciente</th>
-                    <th>Descrição</th>
-                    <th>Médico</th>
-                    <th>Especialidade</th>
-                    <th>Editar</th>
-                    <th>Excluir</th>
-                    <th>Legenda</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($agendamentos as $agendamento)
-                <tr>
-                    <td>{{ $agendamento->id }}</td>
-                    <td>{{ date("d/m/Y H:i:s", strtotime($agendamento->datahora)) }}</td>
-                    <td>{{ $agendamento->patient->name }}</td>
-                    <td>{{ $agendamento->descricao }}</td>
-                                
-                    <td>{{ $agendamento->doctor->name }}</td>
-                    <td>{{ $agendamento->doctor->specialty }}</td>
-                    <td><a href="{{ route('agendamentos.edit', $agendamento->id) }}" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></a></td>
-                        <td>
-                        <form action="{{ route('agendamentos.destroy', $agendamento->id ) }}" method="POST">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                            <button class="btn btn-danger" type="submit"><span class="glyphicon glyphicon-trash"></button>
-                        </form>
-                    </td> 
-                        <td>{{ $agendamento->legenda }}</td>
-                <!-- <td><button class="btn btn-info demo" >Status</button></td> -->        
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @endforeach
 
  <!-- Footer -->
     <footer class="footer bg-light">
