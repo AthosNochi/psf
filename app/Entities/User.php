ae198ff6c9ed9c48ede07f325e89be34cc222412
Athos Nochi <athosnochi@gmail.com>
2020-08-17 21:22:25 -0300

    .

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -19,4 +19,9 @@ class User extends Authenticatable
    protected $fillable     = ['name', 'email', 'password', 'isAdm'];
    protected $hidden       = ['password', 'remember_token'];
 
+   
+   public function setPasswordAttribute ($value)
+   {
+   		$this->attributes['password'] = env('PASSWORD_HASH') ? bcrypt('$value') : '$value';
+   } 
 }
