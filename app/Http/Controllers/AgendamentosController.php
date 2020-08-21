e9f48b3cb9366e9b216b14a3d139be05c6ef9a40
Athos Nochi <athosnochi@gmail.com>
2020-08-21 12:42:12 -0300

    .

--- app/Http/Controllers/AgendamentosController.php
+++ app/Http/Controllers/AgendamentosController.php
@@ -63,7 +63,7 @@ class AgendamentosController extends Controller
         
 
         return view('agendamentos.form', [
-            'name' => $agendamento->name,
+            'name' => $agendamentos->name,
             'agendamentos'      => $agendamentos,
             'patient_list'      => $patient_list,
             'doctor_list'       => $doctor_list,
