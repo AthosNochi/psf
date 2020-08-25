46470e0925589b249b03a610e8dba62f2cf2ca22
Athos Nochi <athosnochi@gmail.com>
2020-08-25 19:28:30 -0300

    .

--- resources/views/psfs/index.blade.php
+++ resources/views/psfs/index.blade.php
@@ -1,10 +1,17 @@
 @extends('templates.master')
 
+@section('css-view')
+@endsection
+
+@section('js-conteudo-view')
+@endsection
+
 @section('conteudo-view')
     @if (session('success'))
         <h3>{{ session('success')['messages'] }}</h3>
     @endif
 
+{!! Form::open(['route' => 'psf.store', 'method' => 'post', 'class' => 'text-center border border-light p-5']) !!}
     <div class="form-group row">
         <label for="name" class="col-sm-4 col-form-label"> Nome:</label>
         <div class="col-sm-8">
@@ -37,6 +44,8 @@
     <div class="modal-footer d-flex justify-content-center">
         <input class="btn btn-primary" type="submit" name="submit" value="Enviar">
     </div>
+
+    {!! Form::close() !!}
 
     <div class="panel">
         <table class="table" align="center">
