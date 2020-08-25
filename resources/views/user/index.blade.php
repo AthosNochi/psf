36896f00a2de797d219947219ad8906619432b01
Athos Nochi <athosnochi@gmail.com>
2020-08-25 17:36:36 -0300

    .

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,25 +1,44 @@
 @extends('templates.master')
 
     <form action="{{ route('user.store') }}" method="post">
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">Email</label>
-            <input type="email" class="form-control" id="email">
-          </div>
-          <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" id="password">
-          </div>
-        </div>
+
         <div class="form-group">
-          <div class="form-check">
-            <input class="form-check-input" type="checkbox" id="isAdm">
-            <label class="form-check-label" for="gridCheck">
-              Administrador
-            </label>
-          </div>
+            <label for="nome">Nome</label>
+            <input id="nome" type="text" name="nome"  value="{{ $paciente->nome }}" class="form-control"/>
         </div>
-        <button type="submit" class="btn btn-primary">C</button>
+
+        <div class="form-group">
+            <label for="rg">RG</label>
+            <input id="rg" type="text" name="rg" value="{{ $user->rg }}" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="cpf">CPF</label>
+            <input id="cpf" type="text" name="cpf" value="{{ $user->cpf }}" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="phone">Telefone</label>
+            <input id="phone" type="text" name="phone" value="{{ $user->phone }}" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" value="{{ $user->email }}" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Email</label>
+            <input id="password" type="password" name="password" value="{{ $user->password }}" class="form-control"/>
+        </div>
+
+        <div class="form-group">
+            <label class="form-check-label" for="gridCheck">
+                Administrador
+            </label>
+        <input id="isAdm"  type="checkbox" name="isAdm" value="{{ $user->isAdm }}" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
 
     <table class="default-table">
