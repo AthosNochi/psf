702cddfebdea57d41665f622197320f846c5e562
Athos Nochi <athosnochi@gmail.com>
2020-08-25 17:42:38 -0300

    .

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,48 +1,24 @@
 @extends('templates.master')
-@extends('login.app')
+
+
+@section('css-view')
+@endsection
+
+@section('js-conteudo-view')
+@endsection
 
 @section('conteudo-view')
+    @if (session('success'))
+        <h3>{{ session('success')['messages'] }}</h3>
+    @endif
+    {!! Form::open(['route' => 'user.store', 'method' => 'post', 'class' => 'form-padrao']) !!}
+        @include('templates.formulario.input', ['input' => 'name', 'attributes' => ['placeholder' => "Nome"]])
+        @include('templates.formulario.input', ['input' => 'email', 'attributes' => ['placeholder' => "Email"]])
+        @include('templates.formulario.password', ['input' => 'password', 'attributes' => ['placeholder' => "Senha"]])
+        @include('templates.formulario.checkbox', ['input' => 'isAdm', 'attributes' => ['placeholder' => "isAdm"]])
 
-    <form action="{{ route('user.store') }}" method="post">
-
-        <div class="form-group">
-            <label for="nome">Nome</label>
-            <input id="nome" type="text" name="nome"  value="{{ $user->name }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="rg">RG</label>
-            <input id="rg" type="text" name="rg" value="{{ $user->rg }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="cpf">CPF</label>
-            <input id="cpf" type="text" name="cpf" value="{{ $user->cpf }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="phone">Telefone</label>
-            <input id="phone" type="text" name="phone" value="{{ $user->phone }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email" value="{{ $user->email }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="password">Email</label>
-            <input id="password" type="password" name="password" value="{{ $user->password }}" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label class="form-check-label" for="gridCheck">
-                Administrador
-            </label>
-        <input id="isAdm"  type="checkbox" name="isAdm" value="{{ $user->isAdm }}" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Cadastrar</button>
-    </form>
+        @include('templates.formulario.submit', ['input' => 'Cadastrar'])    
+    {!! Form::close() !!}
 
     <table class="default-table">
         <thead>
