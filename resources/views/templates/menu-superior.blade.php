5c6818796f26250064177e5e96b673dbef3820a1
Athos Nochi <athosnochi@gmail.com>
2020-08-25 16:23:45 -0300

    ;

--- resources/views/templates/menu-superior.blade.php
+++ resources/views/templates/menu-superior.blade.php
@@ -5,32 +5,8 @@
         <a class="navbar-brand" href="{{ route('psf.index') }}">Psf</a>
         <a class="navbar-brand" href="{{ route('doctor.index') }}">Médicos</a>
         <a class="navbar-brand" href="{{ route('patient.index') }}">Pacientes</a>
+        <a class="navbar-brand" href="{{ route('enfermeiro.index') }}">Efermeiros</a>
+        <a class="navbar-brand" href="{{ route('agendamento.index') }}">Agendamentos</a>
+
     </div>
-      
-        
-        <li>
-            <a href="{{ route('patient.index') }}">
-                <i class"""></i>
-                <h3>Pacientes</h3>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('enfermeiro.index') }}">
-                <i class"""></i>
-                <h3>Enfermeiro</h3>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('agendamento.index') }}">
-                <i class"""></i>
-                <h3>Agendamento</h3>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('logout') }}" class="alert-link">
-                <i class"""></i>
-                <h3>Sair</h3>
-            </a>
-        </li>
-    </ul>
 </nav>
