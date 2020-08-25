414a75c85b6942d5aa754cce1924182c03af5b37
Athos Nochi <athosnochi@gmail.com>
2020-08-25 15:45:18 -0300

    .

--- resources/views/login/app.blade.php
+++ resources/views/login/app.blade.php
@@ -43,12 +43,7 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
+                            
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::User()->name }} <span class="caret"></span>
