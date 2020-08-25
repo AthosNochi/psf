27944b773c8a12cbd7c455999abaac46109f0447
Athos Nochi <athosnochi@gmail.com>
2020-08-25 16:48:23 -0300

    .

--- resources/views/templates/formulario/input.blade.php
+++ resources/views/templates/formulario/input.blade.php
@@ -1,4 +1,4 @@
-<label class="{{ $class ?? null }}">
+<label class="{{ $class ?? "form-control form-control-lg" }}">
     <span>{{ $label ?? $input ?? "erro"}}</span>
     {!! Form::text($input, $value ?? null, $attributes) !!}
 </label>
