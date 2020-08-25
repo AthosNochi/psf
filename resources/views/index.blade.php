40e2688dadee51681222278f34e0c17f25edc7b3
Athos Nochi <athosnochi@gmail.com>
2020-08-25 14:30:59 -0300

    .

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="Sistema de agendamento de consultas">
     <meta name="author" content="">
 
-    <title>Sistema de Agendamento</title>
+    <title>Sistema de Agendamento em PSF</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
