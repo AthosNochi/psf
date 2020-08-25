3d59109194d32a74393e1b1513a936cb274a55b2
Athos Nochi <athosnochi@gmail.com>
2020-08-25 15:46:40 -0300

    .

--- resources/views/login/app.blade.php
+++ resources/views/login/app.blade.php
@@ -43,7 +43,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            
+                            @if (Route::has('register'))
+                            @endif
+                        @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::User()->name }} <span class="caret"></span>
