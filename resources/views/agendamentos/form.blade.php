a7514d69251d476e276e100d292acd63af0e713a
Athos Nochi <athosnochi@gmail.com>
2020-08-26 15:25:03 -0300

    .

--- resources/views/agendamentos/form.blade.php
+++ resources/views/agendamentos/form.blade.php
@@ -46,6 +46,7 @@
               <th>Id</th>
               <th>Descrição</th>
               <th>Data/Hora</th>
+              <td>{{ $agendamento->paciente->nome }}</td>
               
               <th>Legenda</th>
               <th>Opções</th>
