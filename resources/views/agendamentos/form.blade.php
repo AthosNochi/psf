de579333f16fdad971e97c43de1aa39d278161a7
Athos Nochi <athosnochi@gmail.com>
2020-08-26 14:56:44 -0300

    .

--- resources/views/agendamentos/form.blade.php
+++ resources/views/agendamentos/form.blade.php
@@ -17,7 +17,7 @@
 </div>
 <div class="form-group row">
   <label for="paciente_id">Paciente</label>
-  <select name="patient_id" class="form-control" id="patient_id" value="{{ $agendamentos->id_patient }}">
+  <select name="patient_id" class="form-control" id="patient_id" value="{{ $agendamentos->patient_id }}">
     @foreach($patients as $patient)
       <option value="{{ $patient->id }}" {{ $patient->id == $agendamentos->patient_id? 'selected="selected"' : ''}}> {{ $patient->name }} </option>
     @endforeach
