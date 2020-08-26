e55b4b7282edf56728ef4be129cb86b06e9bb7e5
Athos Nochi <athosnochi@gmail.com>
2020-08-26 13:19:21 -0300

    layout doctor

--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -4,24 +4,77 @@
     @if (session('success'))
         <h3>{{ session('success')['messages'] }}</h3>
     @endif
-    {!! Form::open(['route' => 'patient.store', 'method' => 'post', 'class' => 'form-padrao']) !!}
-        @include('templates.formulario.input', ['label' => 'CPF','input' => 'cpf', 'attributes' => ['placeholder' => "CPF"]])
-        @include('templates.formulario.input', ['input' => 'name', 'attributes' => ['placeholder' => "Nome"]])
-        @include('templates.formulario.input', ['input' => 'email', 'attributes' => ['placeholder' => "Email"]])
-        @include('templates.formulario.input', ['input' => 'rg', 'attributes' => ['placeholder' => "RG"]])
-        @include('templates.formulario.input', ['input' => 'phone', 'attributes' => ['placeholder' => "Telefone"]])
-        @include('templates.formulario.input', ['input' => 'birth', 'attributes' => ['placeholder' => "Nascimento"]])
-        @include('templates.formulario.input', ['input' => 'gender', 'attributes' => ['placeholder' => "Genero"]])
-        @include('templates.formulario.input', ['input' => 'notes', 'attributes' => ['placeholder' => "Notas"]])
-        @include('templates.formulario.input', ['input' => 'sus', 'attributes' => ['placeholder' => "Cartão do SUS"]])
-        @include('templates.formulario.input', ['input' => 'address', 'attributes' => ['placeholder' => "Endereço"]])
-
-        @include('templates.formulario.password', ['input' => 'password', 'attributes' => ['placeholder' => "Senha"]])
-        
-        @include('templates.formulario.submit', ['input' => 'Cadastrar'])    
+    {!! Form::open(['route' => 'patient.store', 'method' => 'post', 'class' => 'text-center border border-light p-5']) !!}
+    <div class="form-group row">
+        <label for="name" class="col-sm-4 col-form-label">* Nome:</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="255" name="name" placeholder="Seu nome" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="email" class="col-sm-4 col-form-label">* E-mail:</label>
+        <div class="col-sm-8">
+            <input type="email" class="form-control" maxlength="255" name="email" placeholder="exemplo@email.com" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="password" class="col-sm-4 col-form-label"> Senha:</label>
+        <div class="col-sm-8">
+            <input type="password" class="form-control" maxlength="255" name="password" placeholder="Escolha uma senha" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="cpf" class="col-sm-4 col-form-label">* CPF:</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control" maxlength="11" name="cpf" placeholder="000.000.000-00" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="rg" class="col-sm-4 col-form-label">* RG:</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control" maxlength="9" name="rg" placeholder="00.000.000-0" required>
+        </div>
+    </div> 
+    <div class="form-group row">
+        <label for="birth" class="col-sm-4 col-form-label"> Nascimento:</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="8" name="birth" placeholder="data de nascimento" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="gender" class="col-sm-4 col-form-label"> Genero:</label>
+        <div class="col-sm-5">
+            <input type="text" class="form-control" maxlength="10" name="gender" placeholder="Genero" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="phone" class="col-sm-4 col-form-label">* Telefone:</label>
+        <div class="col-sm-6">
+            <input type="text" class="form-control" maxlength="10" name="phone" placeholder="(99)9999-9999" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="endereco" class="col-sm-4 col-form-label"> Endereço:</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="255" name="endereco" placeholder="Endereço" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="sus" class="col-sm-4 col-form-label"> N° SUS:</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="255" name="sus" placeholder="N° Cartão do SUS" required>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="notes" class="col-sm-4 col-form-label"> Observações:</label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="255" name="notes" placeholder="Diabetes, pressão alta, etc..." required>
+        </div>
+    </div>
     {!! Form::close() !!}
 
-    <table class="default-table">
+    <div class="panel">
+        <table class="table" align="center">
         <thead>
             <tr>
                 <td>#</td>
@@ -61,5 +114,6 @@
             </tr>
             @endforeach
         </tbody>
-    </table>
+        </table>
+    </div>
 @endsection
