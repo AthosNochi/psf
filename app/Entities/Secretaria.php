96f18a2ac3085876f71d3d08fe5f0021aeb6e72f
Athos Nochi <athosnochi@gmail.com>
2020-09-15 15:14:25 -0300

    .

--- app/Entities/Secretaria.php
+++ app/Entities/Secretaria.php
@@ -20,6 +20,29 @@ class Secretaria extends Model implements Transformable
      *
      * @var array
      */
-    protected $fillable = [];
+    protected $fillable = ['cpf', 'name', 'address', 'rg', 'phone', 'email', 'password'];
+
+    public function setPasswordAttribute ($value)
+    {
+            $this->attributes['password'] = env('PASSWORD_HASH') ? bcrypt('$value') : '$value';
+    }
+ 
+    public function getCpfAttribute()
+    {
+         $cpf = $this->attributes['cpf'];
+         return substr($cpf, 0, 3). '.' . substr($cpf, 3, 3). '.' . substr($cpf, 6, 3). '-' . substr($cpf, -2);
+    }
+ 
+    public function getPhoneAttribute()
+    {
+        $phone = $this->attributes['phone'];
+        return "(" . substr($phone, 0, 2) . ")" . substr($phone, 2, 4) . "-" . substr($phone, -4) ;
+    }
+ 
+    public function getRgAttribute()
+    {
+         $rg = $this->attributes['rg'];
+         return substr($rg, 0, 2). '.' . substr($rg, 2, 3). '.' . substr($rg, 5, 3). '-' . substr($rg, -1);
+    }
 
 }
