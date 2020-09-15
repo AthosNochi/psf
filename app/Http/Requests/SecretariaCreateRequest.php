a1889443c8e79ef37d049831d801cdbe601ebcce
Athos Nochi <athosnochi@gmail.com>
2020-09-15 15:10:31 -0300

    .

--- app/Http/Requests/SecretariaCreateRequest.php
+++ app/Http/Requests/SecretariaCreateRequest.php
@@ -13,7 +13,7 @@ class SecretariaCreateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/SecretariaUpdateRequest.php
+++ app/Http/Requests/SecretariaUpdateRequest.php
@@ -13,7 +13,7 @@ class SecretariaUpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
