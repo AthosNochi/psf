bc2199c5777de0fbbe2b134aacb050dd1fab82ea
Athos Nochi <athosnochi@gmail.com>
2020-09-16 13:35:52 -0300

    .

--- resources/views/secretaria/homepage.blade.php
+++ resources/views/secretaria/homepage.blade.php
@@ -30,6 +30,12 @@
             <input type="text" class="form-control" maxlength="255" name="age" placeholder="Insira sua idade" >
         </div>
     </div>
+    <div class="form-group row">
+        <label for="corEtnia" class="col-sm-4 col-form-label"> Cor/etnia: </label>
+        <div class="col-sm-8">
+            <input type="text" class="form-control" maxlength="255" name="corEtnia" placeholder="Insira sua cor/etnia" >
+        </div>
+    </div>
 
     <div class="modal-footer d-flex justify-content-center">
         <input class="btn btn-primary" type="submit" name="submit" value="Enviar">
