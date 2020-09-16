1aee7b1e7df123a450f9061cf214243e7a619049
Athos Nochi <athosnochi@gmail.com>
2020-09-16 13:13:46 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::resource('/secretaria', 'SecretariasController');
 
 Route::resource('/anamnese', 'AnamnesesController');
 
-Route::get('/homepage-Secretaria', 'IndexController@homepageSecretaria');
+Route::post('/homepage-Secretaria', 'IndexController@homepageSecretaria');
 
 Auth::routes();
 
